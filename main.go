edaed01b6c4ff91eaeeeae1b676f764af7869564
Derek Ahn <git.derek@gmail.com>
2019-07-11 14:45:32 -0700

    🚧 Initial plugin capabilites: package create and upload
    - Executes `updateservicectl` to create a new coreupdate package and to upload that package.
    - The uploaded `—file` is comprised of the “filename + version “.tar”, ie “test.1.0.0.tar”; Will make this user configurable in the future.
    - The `—url` is made up of the base url + —file
    
    * TODO = add  compression capabilities prior to file upload

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "0.0.0"
+	build   = "0"
+)
+
+func main() {
+	var flags []cli.Flag
+	for _, a := range args {
+		flags = append(flags, a...)
+	}
+
+	app := cli.NewApp()
+	app.Name = "coreupdate plugin"
+	app.Usage = "coreupdate plugin"
+	app.Action = run
+	app.Version = fmt.Sprintf("%s+%s", version, build)
+	app.Flags = flags
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	plugin := Plugin{
+		Repo: Repo{
+			Owner: c.String("repo.owner"),
+			Name:  c.String("repo.name"),
+		},
+		Build: Build{
+			Tag:      c.String("build.tag"),
+			Number:   c.Int("build.number"),
+			Event:    c.String("build.event"),
+			Status:   c.String("build.status"),
+			Commit:   c.String("commit.sha"),
+			Ref:      c.String("commit.ref"),
+			Branch:   c.String("commit.branch"),
+			Author:   c.String("commit.author"),
+			Pull:     c.String("commit.pull"),
+			Message:  c.String("commit.message"),
+			DeployTo: c.String("build.deployTo"),
+			Link:     c.String("build.link"),
+			Started:  c.Int64("build.started"),
+			Created:  c.Int64("build.created"),
+		},
+		Job: Job{
+			Started: c.Int64("job.started"),
+		},
+		Config: Config{
+			AppID:   c.String("app.id"),
+			Key:     c.String("key"),
+			User:    c.String("user"),
+			Server:  c.String("server"),
+			Version: c.String("pkg.version"),
+			URL:     c.String("pkg.url"),
+			File:    c.String("pkg.file"),
+		},
+	}
+	return plugin.Exec()
+}
---
+++ plugin.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// Plugin TODO
+type Plugin struct {
+	Repo   Repo
+	Build  Build
+	Config Config
+	Job    Job
+}
+
+// Exec plugin (entry)
+func (p Plugin) Exec() error {
+	output, err := p.createPackage()
+	if err != nil {
+		return err
+	}
+	log.Println(string(output))
+
+	output, err = p.uploadPackage()
+	if err != nil {
+		return err
+	}
+	log.Println(string(output))
+
+	return nil
+}
+
+func (p Plugin) createPackage() ([]byte, error) {
+	action := []string{
+		"package",
+		"create",
+		"--app-id=" + p.Config.AppID,
+		"--version=" + p.Config.Version,
+		fmt.Sprintf("--file=%s.%s.tar", p.Config.File, p.Config.Version),
+		fmt.Sprintf("--url=%s/%s.%s.tar",
+			p.Config.URL,
+			p.Config.File,
+			p.Config.Version,
+		),
+	}
+
+	cmd, args := p.baseCMD()
+	args = append(args, action...)
+
+	return exec.Command(cmd, args...).Output()
+}
+
+func (p Plugin) uploadPackage() ([]byte, error) {
+	action := []string{
+		"package",
+		"upload",
+		fmt.Sprintf("--file=%s.%s.tar", p.Config.File, p.Config.Version),
+	}
+
+	cmd, args := p.baseCMD()
+	args = append(args, action...)
+
+	return exec.Command(cmd, args...).Output()
+}
+
+func (p Plugin) baseCMD() (string, []string) {
+	flags := []string{
+		"--key=" + p.Config.Key,
+		"--user=" + p.Config.User,
+		"--server=" + p.Config.Server,
+	}
+	return "updateservicectl", flags
+}
